7e762df24b5eab34a30ebe1c3c30ad2ed1fd7b65
ibrahim-safwat <58376019+ibrahim-safwat@users.noreply.github.com>
2021-08-29 19:58:16 +0200

    task 2 - to be continued

---
+++ Task 2.js
@@ -0,0 +1,91 @@
+// Question 1: True or False 
+
+// 1- True
+// 2- false
+// 3- false
+// 4- true
+// 5- true
+// 6- true
+// 7- false
+// 8- false
+// 9- false
+// 10- false
+// 11- false
+// 12- true 
+
+// question 2: 
+
+// --------------------
+
+// 1
+// 2
+// 3
+// 4
+// 5
+
+// --------------------
+
+// 3
+
+// --------------------
+
+// true
+// (2) [Array(2), Array(2)]
+
+// --------------------
+
+// welcome undefined
+
+// --------------------
+
+//
+
+// --------------------
+
+0
+1
+4
+2
+3
+
+// --------------------
+
+// Question 3: 
+
+// 1 - number 
+
+// 2 - Not a Number, Number, make a function which returns true if the inserted value is a number and NaN if it's not.
+
+// 3 - pattern that requests an event provider and then calls relevant event handler.
+
+// 4 - overriding inherited methods by calling the same methods on different objects. its benefits are reusing codes, easier for debugging, single variables can store multiple values.
+
+
+// Question 4: 
+
+
+
+// Question 5: 
+
+// 1 - 
+
+// var addition = function(a) {
+
+//     return function(b) { 
+
+//     return a + b; 
+// };
+
+// }
+
+// 2 - 
+
+// const arrayOfObject = [{ id: 1, name: 'john' }, {id: 2, name: 'mary'}];
+
+// const checkUsername = obj => obj.name === 'john';
+
+// console.log(true);
+
+// returns true if the name john is found
+
+
